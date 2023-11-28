74f7c159f121ea114d4c392833179a08a39484a5
IkwanThareq <ikwanthareq@gmail.com>
2023-11-28 09:33:18 +0700

    update dmi botsheets icon

--- lib/screens/BottomSheetModal.dart
+++ lib/screens/BottomSheetModal.dart
@@ -1,30 +1,93 @@
 import 'package:flutter/material.dart';
 
-class BottomSheetModal extends StatelessWidget {
+class BottomSheetModal extends StatefulWidget {
+  const BottomSheetModal({super.key});
+
+  @override
+  State<BottomSheetModal> createState() => _BottomSheetModalState();
+}
+
+class _BottomSheetModalState extends State<BottomSheetModal> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      child: Column(
-        children: [
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text('Edit'),
-            onTap: () {
-              // Logika yang ingin Anda jalankan ketika opsi "Edit" dipilih
-              Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.delete),
-            title: Text('Delete'),
-            onTap: () {
-              // Logika yang ingin Anda jalankan ketika opsi "Delete" dipilih
-              Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
-            },
-          ),
-        ],
+    return MaterialApp(
+      title: 'Modal Bottom Sheet Demo',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('Creating a Modal Bottom Sheet'),
+          backgroundColor: Colors.black38,
+        ),
+        body: Builder(
+          builder: (context) {
+            return Center(
+              child: ElevatedButton(
+                child: Text('Show Modal Bottom Sheet'),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Wrap(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.share),
+                            title: Text('Share'),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.copy),
+                            title: Text('Copy Link'),
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.edit),
+                            title: Text('Edit'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
+// class BottomSheetModal extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 500.0,
+//       child: Column(
+//         children: [
+//           ListTile(
+//             leading: Icon(Icons.edit),
+//             title: Text('Edit'),
+//             onTap: () {
+//               // Logika yang ingin Anda jalankan ketika opsi "Edit" dipilih
+//               Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
+//             },
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.delete),
+//             title: Text('Delete'),
+//             onTap: () {
+//               // Logika yang ingin Anda jalankan ketika opsi "Delete" dipilih
+//               Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
+//             },
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.home_filled),
+//             title: Text('Home'),
+//             onTap: () {
+//               // Logika yang ingin Anda jalankan ketika opsi "Delete" dipilih
+//               Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/screens/MainMenu.dart
+++ lib/screens/MainMenu.dart
@@ -2,6 +2,7 @@ import 'package:fe_dmi/screens/BottomSheetModal.dart';
 import 'package:fe_dmi/screens/HomeScreen.dart';
 import 'package:fe_dmi/screens/LoginScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 // ini code yang bene sebelumnya
 class MainMenu extends StatefulWidget {
@@ -56,7 +57,6 @@ class _MainMenuState extends State<MainMenu> {
                 icon: Icon(Icons.home),
                 label: "Home",
               ),
-              
               BottomNavigationBarItem(
                 icon: Icon(Icons.person),
                 label: "Login",
@@ -78,33 +78,308 @@ class _MainMenuState extends State<MainMenu> {
   }
 }
 
+// ini code yang baru
 void _showModalBottomSheet(BuildContext context) {
   showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {
-      return Container(
-        height: 200,
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Edit'),
-              onTap: () {
-                // Logika yang ingin Anda jalankan ketika opsi "Edit" dipilih
-                Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.delete),
-              title: Text('Delete'),
-              onTap: () {
-                // Logika yang ingin Anda jalankan ketika opsi "Delete" dipilih
-                Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
-              },
-            ),
-          ],
+      return SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          // height: 500.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Sholat dan Navigasi',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/kaaba.png'),
+                    label: 'Arah Kiblat',
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: 'GeeksforGeeks',
+                        backgroundColor: Colors.grey,
+                      );
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/shalat.png'),
+                    label: 'Jadwal Shalat',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Camera di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/halal.png'),
+                    label: 'Restoran Halal',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Music di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/cloud.png'),
+                    label: 'Kondisi cuaca',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Location di sini
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Masjid dan Kegiatan',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/kaaba.png'),
+                    label: 'Arah Kiblat',
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: 'GeeksforGeeks',
+                        backgroundColor: Colors.grey,
+                      );
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/shalat.png'),
+                    label: 'Jadwal Shalat',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Camera di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/halal.png'),
+                    label: 'Restoran Halal',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Music di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/cloud.png'),
+                    label: 'Kondisi cuaca',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Location di sini
+                    },
+                  ),
+                ],
+              ),
+              
+              const SizedBox(height: 16.0),
+              const Text(
+                'Halo Dai',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/kaaba.png'),
+                    label: 'Arah Kiblat',
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: 'GeeksforGeeks',
+                        backgroundColor: Colors.grey,
+                      );
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/shalat.png'),
+                    label: 'Jadwal Shalat',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Camera di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/halal.png'),
+                    label: 'Restoran Halal',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Music di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/cloud.png'),
+                    label: 'Kondisi cuaca',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Location di sini
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/kaaba.png'),
+                    label: 'Arah Kiblat',
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: 'GeeksforGeeks',
+                        backgroundColor: Colors.grey,
+                      );
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/shalat.png'),
+                    label: 'Jadwal Shalat',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Camera di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/halal.png'),
+                    label: 'Restoran Halal',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Music di sini
+                    },
+                  ),
+                  _buildRoundedCard(
+                    icon: Image.asset('assets/images/cloud.png'),
+                    label: 'Kondisi cuaca',
+                    onPressed: () {
+                      // Tambahkan logika untuk menampilkan halaman Location di sini
+                    },
+                  ),
+                ],
+              ),
+              
+            ],
+          ),
         ),
       );
     },
   );
 }
+
+Widget _buildRoundedCard({required Widget icon, required label, required onPressed}) {
+  return GestureDetector(
+    onTap: onPressed,
+    child: Column(
+      children: [
+        Container(
+          width: 70.0,
+          height: 70.0,
+          decoration: BoxDecoration(
+            color: Colors.blue, // Ubah warna sesuai keinginan Anda
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Center(
+            child: icon, // Menggunakan Widget yang diberikan sebagai ikon
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          label,
+          style: TextStyle(fontSize: 14.0),
+        ),
+      ],
+    ),
+  );
+}
+
+
+
+// void _showModalBottomSheet(BuildContext context) {
+//   showModalBottomSheet(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return Container(
+//         padding: EdgeInsets.all(16.0),
+//         height: 500.0,
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             const Text(
+//               'Judul Konten',
+//               style: TextStyle(
+//                 fontSize: 18.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             const SizedBox(height: 16.0),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 _buildRoundedCard(Icons.card_giftcard),
+//                 _buildRoundedCard(Icons.camera),
+//                 _buildRoundedCard(Icons.music_note),
+//                 _buildRoundedCard(Icons.location_on),
+//               ],
+//             ),
+//             const SizedBox(height: 16.0),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }
+
+// Widget _buildRoundedCard(IconData icon) {
+//   return Container(
+//     width: 70.0,
+//     height: 70.0,
+//     decoration: BoxDecoration(
+//       color: Colors.blue, // Ubah warna sesuai keinginan Anda
+//       borderRadius: BorderRadius.circular(10.0),
+//     ),
+//     child: Center(
+//       child: Icon(
+//         icon,
+//         size: 36.0,
+//         color: Colors.white,
+//       ),
+//     ),
+//   );
+// }
+
+
+// void _showModalBottomSheet(BuildContext context) {
+//   showModalBottomSheet(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return Container(
+//         height: 500.0,
+//         child: Column(
+//           children: [
+//             ListTile(
+//               leading: Icon(Icons.edit),
+//               title: Text('Edit'),
+//               onTap: () {
+//                 // Logika yang ingin Anda jalankan ketika opsi "Edit" dipilih
+//                 Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
+//               },
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.delete),
+//               title: Text('Delete'),
+//               onTap: () {
+//                 // Logika yang ingin Anda jalankan ketika opsi "Delete" dipilih
+//                 Navigator.pop(context); // Menutup Bottom Sheet setelah dipilih
+//               },
+//             ),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }
